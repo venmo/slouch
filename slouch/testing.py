89724d02cb92f35d7e04b07cfb8def5eb1df49cb
Nina Zakharenko <nzakharenko@gmail.com>
2016-10-20 09:20:09 -0600

    Add the ability to send bot commands with or without a `:` in the @ mention (#7)
    * add a method to figure out how the bot was addressed, extract the command based on this identifier
    
    * remove unneeded _sent_to_me method
    
    * introduce a simple unit test to ensure both styles of message sending work as expected
    
    * fix typo, assert that the message looks like we expect it to

--- slouch/testing.py
+++ slouch/testing.py
@@ -33,7 +33,7 @@ class CommandTestCase(TestCase):
     def tearDown(self):
         self.slack_patcher.stop()
 
-    def send_message(self, command, **event):
+    def send_message(self, command, message_delimiter=':', **event):
         """Return the bot's response to a given command.
 
         :param command: the message to the bot.
@@ -44,9 +44,12 @@ class CommandTestCase(TestCase):
 
         _event = {
             'type': 'message',
-            'text': "%s: %s" % (self.bot.name, command),
+            'text': "%s%s%s" % (self.bot.name, message_delimiter, command),
             'channel': None,
         }
+
+        self.assertTrue(_event['text'].startswith("%s%s" % (self.bot.name, message_delimiter)))
+
         _event.update(event)
 
         self.bot._on_message(self.ws, json.dumps(_event))
